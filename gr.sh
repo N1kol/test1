329173aeae41c01ee0e37c21dcd4004a852cbea0
N1kol <39703205+N1kol@users.noreply.github.com>
2022-09-29 12:12:49 +0400

    Обновить

--- gr.sh
+++ gr.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-apt -y update&& apt -y dist-upgrade && apt -y full-upgrade && touch 5.txt && echo "hui" >> 5.txt && cat 5.txt
+apt -y update&& apt -y dist-upgrade && apt -y full-upgrade 
